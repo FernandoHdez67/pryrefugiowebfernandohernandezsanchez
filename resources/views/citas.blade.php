9115f946cb3fb69502f52f7ac6f5da5154fb30d3
FernandoHdez67 <20200767@uthh.edu.mx>
2023-02-15 18:40:54 -0600

    Creacion de vista citas Fernando

---
+++ resources/views/citas.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Citas</title>
+</head>
+<body>
+    <div class="mb-3">
+      <label for="" class="form-label">Nombre</label>
+      <label for="" class="form-label">Apellidos</label>
+      <label for="" class="form-label">Usuario</label>
+      <input type="submit" value="Enviar">
+    </div>
+</body>
+</html>
